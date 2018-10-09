c669a156e5f5d5d9d269bf3dc65d352f05a806e3
Theepan Thevathasan <nrotechmonk@gmail.com>
2018-10-09 22:44:14 +1100

    routes

--- src/movie.model.js
+++ src/movie.model.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let MovieSchema = new Schema({
-  name: { type: String, required: true, max: 100 },
-  price: { type: Number, required: true }
+  name: { type: String, required: true, max: 100 }
+  // ,
+  // price: { type: Number, required: true }
 });
 
 // Export the model

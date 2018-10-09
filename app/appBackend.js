9fd9788b86e701f3e8102b4aacfaafcbc5be3e86
Theepan Thevathasan <nrotechmonk@gmail.com>
2018-10-10 03:42:33 +1100

    dotenv

--- app/appBackend.js
+++ app/appBackend.js
@@ -1,15 +1,25 @@
 "use strict";
 
-// app.js
+var _express = require("express");
 
-var express = require("express");
-var bodyParser = require("body-parser");
+var _express2 = _interopRequireDefault(_express);
 
-//import appwide routes
-var mainRoutes = require("./app.route");
-var movieRoutes = require("./movie.route");
+var _bodyParser = require("body-parser");
+
+var _app = require("./app.route");
+
+var _app2 = _interopRequireDefault(_app);
+
+var _movie = require("./movie.route");
+
+var _movie2 = _interopRequireDefault(_movie);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // Set up mongoose connection
+
+
+//import appwide routes
 var mongoose = require("mongoose");
 
 var dev_db_url = "mongodb://movieapp:movieapp123@ds245150.mlab.com:45150/movie-app-db";
@@ -25,14 +35,21 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 // });
 
 //Web Server
-var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+var app = (0, _express2.default)();
+app.use((0, _bodyParser.json)());
+app.use((0, _bodyParser.urlencoded)({ extended: false }));
 //app.use("/", mainRoutes);
-app.use("/movie", movieRoutes);
+app.use("/movie", _movie2.default);
 
 var port = 1234;
 
 app.listen(port, function () {
   console.log("Movie App server is up and running on port numner " + port);
 });
+
+//let envresult = require("dotenv").config({path: "../.env", debug: process.env.DEBUG}); //module to load ENV variables
+
+//console.log(envresult.parsed);
+
+console.log(process.env.DB_PASSWORD); // baconpancakes
+console.log(process.env.DB_USER); // root
--- src/appBackend.js
+++ src/appBackend.js
@@ -1,14 +1,9 @@
-// app.js
-
-const express = require("express");
-const bodyParser = require("body-parser");
-
+import express from "express";
+import { json, urlencoded } from "body-parser";
 
 //import appwide routes
-const mainRoutes = require("./app.route");
-const movieRoutes = require("./movie.route");
-
-
+import mainRoutes from "./app.route";
+import movieRoutes from "./movie.route";
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -16,7 +11,10 @@ const mongoose = require("mongoose");
 let dev_db_url =
   "mongodb://movieapp:movieapp123@ds245150.mlab.com:45150/movie-app-db";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(
+  mongoDB,
+  { useNewUrlParser: true }
+);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
@@ -28,8 +26,8 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 //Web Server
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 //app.use("/", mainRoutes);
 app.use("/movie", movieRoutes);
 
@@ -38,3 +36,10 @@ let port = 1234;
 app.listen(port, () => {
   console.log("Movie App server is up and running on port numner " + port);
 });
+
+//let envresult = require("dotenv").config({path: "../.env", debug: process.env.DEBUG}); //module to load ENV variables
+
+//console.log(envresult.parsed);
+
+console.log(process.env.DB_PASSWORD) // baconpancakes
+console.log(process.env.DB_USER) // root

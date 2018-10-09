800b56f74768f0243732810f307cdba710129c16
Theepan Thevathasan <nrotechmonk@gmail.com>
2018-10-09 20:28:54 +1100

    updated folder org

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // @gokuvinoth
 // 03/Oct/2018 Initial Draft
 // 07/Oct/2018 modified as per the comments to simply code
@@ -6,9 +8,9 @@
 
 // Calling the movie result api
 
-const xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 xhr.responseType = "json";
-xhr.onreadystatechange = () => {
+xhr.onreadystatechange = function () {
   if (xhr.readyState === XMLHttpRequest.DONE) {
     console.log(xhr.response);
     // function call to process api result
@@ -19,33 +21,19 @@ xhr.onreadystatechange = () => {
 xhr.open("GET", "https://salty-sea-40504.herokuapp.com/movies");
 xhr.send();
 
-displayResult = response => {
-  let responseDataArry = response;
+displayResult = function displayResult(response) {
+  var responseDataArry = response;
   console.log(responseDataArry[0]);
 
-  let movieElement = "";
+  var movieElement = "";
   //   looping to create elements for number of items returned
-  responseDataArry.forEach(function(responseData) {
-    movieElement =
-      movieElement +
-      `<div class="movie">
-    <h2 class="movie-title" id="movietitle">${responseData.title}</h2>
-    <img class="movie-poster" id="movieposter" src="${
-      responseData.poster
-    }" alt="hero and heroine">
-    <ul class="movie-info" id="movieinfo">
-        <li class="movie-show-time" id=movieshowtime><a class="movie-more-info" id="moviemoreinfo" href="#">${
-          responseData.sessions[0].sessionDateTime[0]
-        }</a></li>
-        <li class="movie-language" id="movielanguage">${
-          responseData.language
-        }</li>
-    </ul>
-</div>`;
+  responseDataArry.forEach(function (responseData) {
+    movieElement = movieElement + ("<div class=\"movie\">\n    <h2 class=\"movie-title\" id=\"movietitle\">" + responseData.title + "</h2>\n    <img class=\"movie-poster\" id=\"movieposter\" src=\"" + responseData.poster + "\" alt=\"hero and heroine\">\n    <ul class=\"movie-info\" id=\"movieinfo\">\n        <li class=\"movie-show-time\" id=movieshowtime><a class=\"movie-more-info\" id=\"moviemoreinfo\" href=\"#\">" + responseData.sessions[0].sessionDateTime[0] + "</a></li>\n        <li class=\"movie-language\" id=\"movielanguage\">" + responseData.language + "</li>\n    </ul>\n</div>");
 
     console.log(movieElement);
   });
   //   create final element and append to display the full content in HTML
-  let resultElement = document.querySelector("#movies");
+  var resultElement = document.querySelector("#movies");
   resultElement.innerHTML = movieElement;
 };
+//# sourceMappingURL=app.js.map
